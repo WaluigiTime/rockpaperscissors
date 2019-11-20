856364b408e0516e400e8a1530ae7a8c227a05a5
WaluigiTime <wahthymebackup@gmail.com>
2019-11-20 22:00:50 +0000

    help

--- js/script.js
+++ js/script.js
@@ -9,7 +9,23 @@ $("#shoot").click(function(){
    let userChoice = $("input").val();
    $("choice").html(`<li> ${userChoice} </li>`)
 
-if
+   let computerChoice = Math.random();
+
+    
+   {
+    if (computerChoice < 0.33 && computerChoice < 0.0) {
+        $("#text").html("You lose!");
+    
+
+    } else if (computerChoice < 0.33 && computerChoice < 0.66) {
+      $("#text").html("You win!")
+
+    } else (computerChoice < 0.66 && computerChoice < 1.0)
+      $("#text").html("That's a tie bruh.")
+
+      
+    }
+
 });
 
 
